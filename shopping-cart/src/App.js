93a75eb5fa4d994b43754ceae77c3d5d21ea0617
junting <342766475@qq.com>
2021-03-08 20:36:21 +0800

    静态页面准备完毕

---
+++ shopping-cart/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Product from './components/Product'
+import Cart from './components/Cart'
+import './app.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Product></Product>
+      <Cart></Cart>
+    </div>
+  );
+}
+
+export default App;
