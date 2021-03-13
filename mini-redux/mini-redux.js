b48fa72ca7baa54b4ef1a91ae240476cf01b9c1c
junting <342766475@qq.com>
2021-03-13 11:46:26 +0800

    ReduxAndMobx | mini-redux 的核心功能实现

--- mini-redux/mini-redux.js
+++ mini-redux/mini-redux.js
@@ -6,13 +6,15 @@
 function createStore(reducer, preloadedState) {
   // 当前存储的初始状态
   let currentState = preloadedState
-  // 存放订阅者
+  // 存放订阅者的消息队列
   const listenerQueue = []
 
+  // 获取状态
   function getState () {
     return currentState
   }
 
+  // 触发器 - 触发 action
   function dispatch (action) {
     currentState = reducer(currentState, action)
 
@@ -23,6 +25,7 @@ function createStore(reducer, preloadedState) {
     }
   }
 
+  // 订阅状态 => 状态变更后执行回调函数 fn， fn 存放在订阅者消息列表
   function subscribe (fn) {
     listenerQueue.push(fn)
   }
